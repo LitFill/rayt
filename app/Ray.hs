93a4e9a4ed1db1ab8cfa8bb9092b23fdd9376541
LitFill <marrazzy54@gmail.com>
2026-04-29 00:32:28 +0700

    feat(Ray): add HitInfo, Hittable a, Sphere

--- app/Ray.hs
+++ app/Ray.hs
@@ -40,3 +40,50 @@ hitSphere center radius ray
     h = ray.direction · oc
     c = lenSquared oc - radius * radius
     discriminant = h * h - a * c
+
+
+data HitInfo = HitInfo
+    { hitPoint :: Vec3
+    , hitNormal :: Vec3
+    , hitT :: Double
+    }
+    deriving (Show)
+
+
+class Hittable obj where
+    hit :: Ray -> Double -> Double -> obj -> Maybe HitInfo
+
+
+data Sphere = Sphere
+    { sphereCenter :: Vec3
+    , sphereRadius :: Double
+    }
+    deriving (Show)
+
+
+instance Hittable Sphere where
+    hit ray tmin tmax (Sphere center radius)
+        | determinant < 0 = Nothing
+        | root <= tmin || root >= tmax = Nothing
+        | otherwise = Just info
+      where
+        oc = center - ray.origin
+        a = ray & direction & lenSquared
+        h = ray.direction · oc
+        c = lenSquared oc - radius * radius
+
+        determinant = h * h - a * c
+
+        sqrtd = sqrt determinant
+        root' = (h - sqrtd) / a
+        root = if root' <= tmin || root' >= tmax then (h + sqrtd) / a else root'
+
+        t = root
+        p = ray & rayAt t
+        normal = (p - center) /^ radius
+        info =
+            HitInfo
+                { hitT = t
+                , hitPoint = p
+                , hitNormal = normal
+                }
