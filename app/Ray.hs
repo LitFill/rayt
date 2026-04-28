c5968759c3e1674c1eddfd6911307232f93523f6
LitFill <marrazzy54@gmail.com>
2026-04-28 19:27:02 +0700

    refactor(Ray): use guard pattern

--- app/Ray.hs
+++ app/Ray.hs
@@ -16,22 +16,19 @@ rayAt t (Ray og dr) = og + scale t dr
 
 
 rayColor :: Ray -> Color
-rayColor r =
-    if isHitSphere (Vec3 0 0 (-1)) 0.5 r
-        then Vec3 1 0 0
-        else
-            let normDir = normalize r.direction
-                a = 0.5 * (normDir.y + 1)
-             in scale (1 - a) (fromOne 0.9) + scale a (Vec3 0.5 0.75 1)
+rayColor r
+    | isHitSphere (Vec3 0 0 (-1)) 0.5 r = Vec3 1 0 0
+    | otherwise =
+        let normDir = normalize r.direction
+            a = 0.5 * (normDir.y + 1)
+         in scale (1 - a) (fromOne 0.9) + scale a (Vec3 0.5 0.75 1)
 
 
 isHitSphere :: Vec3 -> Double -> Ray -> Bool
 isHitSphere center radius ray =
-    let
-        oc = center - ray.origin
+    let oc = center - ray.origin
         a = ray.direction · ray.direction
         b = (-2) * ray.direction · oc
         c = oc · oc - radius * radius
         discriminant = b * b - 4 * a * c
-     in
-        discriminant >= 0
+     in discriminant >= 0
