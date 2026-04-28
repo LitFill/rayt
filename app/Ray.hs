447df4f5107056878a2e26e8019146ec9c311e84
LitFill <marrazzy54@gmail.com>
2026-04-28 23:24:18 +0700

    feat(Ray): add normal vector

--- app/Ray.hs
+++ app/Ray.hs
@@ -3,6 +3,8 @@
 
 module Ray where
 
+import Data.Function ((&))
+
 import Vec3
 
 
@@ -17,18 +19,24 @@ rayAt t (Ray og dr) = og + scale t dr
 
 rayColor :: Ray -> Color
 rayColor r
-    | isHitSphere (Vec3 0 0 (-1)) 0.5 r = Vec3 1 0 0
+    | t > 0 =
+        let n = normalize (rayAt t r - Vec3 0 0 (-1))
+         in scale 0.5 (n & v3MapAll (+ 1))
     | otherwise =
         let normDir = normalize r.direction
             a = 0.5 * (normDir.y + 1)
          in scale (1 - a) (fromOne 0.9) + scale a (Vec3 0.5 0.75 1)
+  where
+    t = hitSphere (Vec3 0 0 (-1)) 0.5 r
 
 
-isHitSphere :: Vec3 -> Double -> Ray -> Bool
-isHitSphere center radius ray =
-    let oc = center - ray.origin
-        a = ray.direction · ray.direction
-        b = (-2) * ray.direction · oc
-        c = oc · oc - radius * radius
-        discriminant = b * b - 4 * a * c
-     in discriminant >= 0
+hitSphere :: Vec3 -> Double -> Ray -> Double
+hitSphere center radius ray
+    | discriminant < 0 = -1
+    | otherwise = (-b - sqrt discriminant) / (2 * a)
+  where
+    oc = center - ray.origin
+    a = ray.direction · ray.direction
+    b = (-2) * ray.direction · oc
+    c = oc · oc - radius * radius
+    discriminant = b * b - 4 * a * c
